2a60c57426e06532e15091ad81b10d300c7d7bbc
Eric <Ericfrancis313@gmail.com>
2023-05-09 22:50:43 -0400

    Working solution

---
+++ 8th-Kyu/5 without numbers!!.js
@@ -0,0 +1,7 @@
+//Write a function that always returns 5Sounds easy right? 
+//Just bear in mind that you can't use any of the following characters: 0123456789*+-/ 
+//Good luck :)
+function unusualFive() {
+  let string = "fivee"
+  return string.length
+}
